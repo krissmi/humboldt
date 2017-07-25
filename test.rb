2bce6494a3068d1caf9616b8c20b640d0ec464ca
Krishna Smith <krishna@sphenisc.us>
2017-07-25 16:05:07 -0700

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,9 +1,9 @@
 require octokit
 
-> if ARGV.empty?
->  puts "Post new issues or, if given an issue id, add a comment."
->  puts "Usage: gh-simple-issue REPO [ISSUEID] <text>"
->  exit 1
-> end
->
-> This is a comment
+# > if ARGV.empty?
+# >  puts "Post new issues or, if given an issue id, add a comment."
+# >  puts "Usage: gh-simple-issue REPO [ISSUEID] <text>"
+# >  exit 1
+# > end
+# >
+# > This is a comment
